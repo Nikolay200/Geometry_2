8146fa915a0caba7a7035026b5b81bc68a22eadc
Nikolay <kolekdoma@mail.ru>
2021-07-31 18:54:58 +0300

    Создал MainForm

---
+++ Geometry_2_WindowsFormsApp/MainForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace Geometry_2_WindowsFormsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CircleButton = new System.Windows.Forms.Button();
+            this.RectangleButton = new System.Windows.Forms.Button();
+            this.TriangleButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CircleButton
+            // 
+            this.CircleButton.Location = new System.Drawing.Point(73, 75);
+            this.CircleButton.Name = "CircleButton";
+            this.CircleButton.Size = new System.Drawing.Size(107, 53);
+            this.CircleButton.TabIndex = 0;
+            this.CircleButton.Text = "Круг";
+            this.CircleButton.UseVisualStyleBackColor = true;
+            // 
+            // RectangleButton
+            // 
+            this.RectangleButton.Location = new System.Drawing.Point(74, 160);
+            this.RectangleButton.Name = "RectangleButton";
+            this.RectangleButton.Size = new System.Drawing.Size(106, 53);
+            this.RectangleButton.TabIndex = 1;
+            this.RectangleButton.Text = "Прямоугольник";
+            this.RectangleButton.UseVisualStyleBackColor = true;
+            // 
+            // TriangleButton
+            // 
+            this.TriangleButton.Location = new System.Drawing.Point(75, 255);
+            this.TriangleButton.Name = "TriangleButton";
+            this.TriangleButton.Size = new System.Drawing.Size(105, 53);
+            this.TriangleButton.TabIndex = 2;
+            this.TriangleButton.Text = "Треугольник";
+            this.TriangleButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 450);
+            this.Controls.Add(this.TriangleButton);
+            this.Controls.Add(this.RectangleButton);
+            this.Controls.Add(this.CircleButton);
+            this.Name = "MainForm";
+            this.Text = "Геометрия";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CircleButton;
+        private System.Windows.Forms.Button RectangleButton;
+        private System.Windows.Forms.Button TriangleButton;
+    }
+}
+
